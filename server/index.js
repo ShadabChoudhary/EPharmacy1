77feeeeea9b86ba67001f0aa433ff221c9ae33d7
ShadabChoudhary <shaddychoudhary21@gmail.com>
2022-05-09 13:23:43 +0530

    added production code in server index

--- server/index.js
+++ server/index.js
@@ -29,13 +29,13 @@ Connectdb(username, password);
 app.use("/api", web);
 app.use("/api", registerRoute);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join("client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join("client/build")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
